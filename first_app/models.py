9560fad9900bf905b8e31a8676bdf5282bd11787
evenyan <evenyan@evenyandeMacBook-Pro.local>
2020-09-15 16:37:24 +0800

    注册模型

--- first_app/models.py
+++ first_app/models.py
@@ -5,3 +5,6 @@ class UserInfo(models.Model):
     name = models.CharField(max_length=100)
     age = models.IntegerField()
     password = models.CharField(max_length=400)
+
+    def __str__(self):
+        return self.name
