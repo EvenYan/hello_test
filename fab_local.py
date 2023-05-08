e87da80b4129ba27f20f1d122122a811a105b4aa
Even <1014934663@qq.com>
2023-05-08 23:15:49 +0800

    init

--- fab_local.py
+++ fab_local.py
@@ -4,4 +4,6 @@ def prepare_deploy():
     local("python3 manage.py makemigrations")
     local("python3 manage.py migrate")
     local("git add . && git commit -m 'init'")
-    local("git push")
+    # 需提前在github创建hello_test仓库
+    local("git remote add origin git@github.com:EvenYan/hello_test.git")
+    local("git push -u origin master -f")
