84d3057ef58807f6eba2a92cba2206548bcb6966
Even <1014934663@qq.com>
2023-05-09 08:09:17 +0800

    init

--- fab_local.py
+++ fab_local.py
@@ -1,9 +1,12 @@
 from fabric.api import *
+from fabric.contrib.console import confirm
 
 def prepare_deploy():
     local("python3 manage.py makemigrations")
     local("python3 manage.py migrate")
     local("git add . && git commit -m 'init'")
     # 需提前在github创建hello_test仓库
-    # local("git remote add origin git@github.com:EvenYan/hello_test.git")
-    local("git push -u origin master -f")
+    with settings(warn_only=True):
+        result = local("git remote add origin git@github.com:EvenYan/hello_test.git")
+    if result.failed and confirm("继续还是中断？"):
+        local("git push -u origin master -f")
