813a01945ca97a9d5ce3687a674319e845b5147e
Even <1014934663@qq.com>
2023-05-09 09:20:22 +0800

    init

--- fab_local.py
+++ fab_local.py
@@ -1,10 +1,22 @@
 from fabric.api import *
 from fabric.contrib.console import confirm
 
+
+env.user='root'
+# env.host='172.16.2.142'
+env.hosts=['172.16.2.142', "172.16.2.141"]
+env.exclude_hosts=['172.16.2.141']
+env.password='!web123@'
+env.port="22"
+env.passwords={
+    'root@172.16.2.142':'!web123@',
+    'normal@172.16.2.132':'abc',
+}
+
 def db():
     local("python3 manage.py makemigrations")
     local("python3 manage.py migrate")
-    local("pip freeze > requirements.txt")
+    # local("pip freeze > requirements.txt")
 
 
 def git():
@@ -17,9 +29,19 @@ def add_origin():
     if result.failed and confirm("继续或中断？"):
         pass
 
+
 def push():
     result = local("git push -u origin master -f", capture=True)
     print(result)
+
+def pull():
+    with cd("/root"):
+        run("git clone git@github.com:EvenYan/hello_test.git")
+
+def runserver():
+    with cd("/root/hello_test"):
+        run("pip install -r requirements.txt")
+        run("python3 manage.py runserver")
 
 
 def prepare_deploy():
@@ -27,3 +49,8 @@ def prepare_deploy():
     execute(git)
     execute(add_origin)
     execute(push)
+
+
+def deploy():
+    execute(pull)
+    execute(runserver)
